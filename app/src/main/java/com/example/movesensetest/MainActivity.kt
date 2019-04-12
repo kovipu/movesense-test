341358f169b8d35e85659a076d8407b62601be63
Konsta Purtsi <kovipu@utu.fi>
2019-04-12 16:18:47 +0300

    Initialize Android app

---
+++ app/src/main/java/com/example/movesensetest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.movesensetest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
